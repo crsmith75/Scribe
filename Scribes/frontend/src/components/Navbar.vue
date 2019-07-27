7b3fa606194c2fcf23cba99bf0377d0bbaf6eec8
Connor Smith <connorsmith@connors-mbp-2.dhcp.nd.edu>
2019-07-27 14:49:12 -0400

    added navbar and material libraries

--- Scribes/frontend/src/components/Navbar.vue
+++ Scribes/frontend/src/components/Navbar.vue
@@ -1,15 +1,36 @@
 
 <template>
  
-  <div class="navbar-fixed">
-    <nav>
-      <div class="nav-wrapper">
-        <a href="#!" class="left brand-logo">Logo</a>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-        </ul>
-      </div>
+  <div class="navbar-fixed ">
+    <nav class="my-navbar">
+        <div class="container">
+            <div class="nav-wrapper">
+
+                 <router-link
+                        :to="{ name: 'home' }"
+                        class="left brand-logo"
+                    ><span class="white-text">Facticus</span>
+                    </router-link>
+                <ul class="right hide-on-med-and-down">
+                <li>
+                    <router-link
+                        :to="{ name: 'home' }"
+                        class="waves-effect waves-light btn"
+                    >Home
+                    </router-link>
+                </li>
+                <li><router-link
+                        :to="{ name: 'about' }"
+                        class="waves-effect waves-light btn"
+                    >About
+                    </router-link>
+                </li>
+                <li>
+                    <a class="waves-effect waves-light btn" href="/accounts/logout/">Logout</a>
+                </li>
+                </ul>
+            </div>
+        </div>
     </nav>
   </div>
  
@@ -25,5 +46,22 @@ export default {
 
 
 <style>
+.my-navbar {
+  background-color: #29b6f6  !important;
+}
+.item {
+  background-color: white;
+}
+.btn {
+  background-color: white;
+}
+.brand-logo {
+    font-weight: bold;
+    font-size: 130%;
+    color: white;
+}
+.brand-logo:hover {
+    color: #f56f12 !important;
+}
 
 </style>
